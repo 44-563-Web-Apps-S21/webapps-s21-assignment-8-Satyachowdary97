844c060bef9880f03b02d94a88a1727c67e05bb1
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2021-03-15 16:05:11 -0500

    Updated app2.js
    Added a table from assignment 3 part 8.

--- app2.js
+++ app2.js
@@ -1,3 +1,4 @@
+
 const http = require('http')    //Pull in a useful node package
                                 
 const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
@@ -13,7 +14,33 @@ const server =
       res.write('<body>')
       res.write('Content \n')
       res.write('More content \n')
-      res.write('Hello World')    
+      res.write('Hello World')
+      res.write('<table>')
+      res.write('<tr>')
+      res.write('<th>Location</th>')
+      res.write('<th>Number of hours that a visitor should spend</th>')
+      res.write('<th>Amount to be spent</th>')
+      res.write('</tr>')
+      res.write('<tr>')
+      res.write('<td>Goa</td>')
+      res.write('<td>150</td>')
+      res.write('<td>$ 200</td>')
+      res.write('</tr>')
+      res.write('<td>Pondicherry</td>')
+      res.write('<td>75</td>')
+      res.write('<td>$ 150</td>')
+      res.write('</tr>')
+      res.write('<tr>')
+      res.write('<td>Vizag</td>')
+      res.write('<td>100</td>')
+      res.write('<td>$ 100</td>')
+      res.write('</tr>')
+      res.write('<tr>')
+      res.write('<td>Mumbai</td>')
+      res.write('<td>50</td>')
+      res.write('<td>$ 250</td>')
+      res.write('</tr>')
+      res.write('</table>')
       res.end('</body></html>')
       //Close the response
     }                           
