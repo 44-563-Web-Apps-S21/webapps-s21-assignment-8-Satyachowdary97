b029452352428a1632c7f2adf4aaa44f601d2848
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2021-03-17 23:11:40 -0500

    Added bonus.js
    Added bonus.js file.

--- bonus.js
+++ bonus.js
@@ -1,17 +1,37 @@
 const http = require('http')    //Pull in a useful node package
-//Try http.
-const hostname = '127.0.0.1'    //Local host
-const port = 3001               //Not assigned
+
+
+const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
+const port = process.env.port || 3001               //and the port
 
 const server =
-http.createServer(            //Creates the response loop
-(req,res)=> {               //Anonymous function to handle the request
-res.statusCode = 200      //code for OK
-res.setHeader('Content-Type', 'text/plain') //Set the mime type
-res.end('I love playing indoor and outdoor games')    //Close the response and provide content
-}                           //No return needed, we modified the res object we got
-)
+    http.createServer(            //Creates the response loop
+    (req, res) => {               //Anonymous function to handle the request
+    console.log("Request recieved")
+    // console.log(req.url)
+    // Build a fake url so we can get the search parameters using a URL object
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    if (req.method === 'GET') {
+
+    //   console.log("Look for query parameter data: " + search_params.get("data"))
+    const Vegetables = ["Carrot","Potato","Raddish","Beetroot","Capsicum","Broccoli","Tomato","Cabbage"]
+    let randomIndex = Math.floor(Math.random() * Vegetables.length)
+    // Process the queries here
+    res.statusCode = 200      //code for OK
+    res.setHeader('Content-Type', 'text/plain')
+    res.write(`${Vegetables[randomIndex]}`)
+    res.end();
+
+    } else {
+    console.log("Status 404")
+    res.statusCode = 404;
+    res.end();
+    }
+
+    }
+    )
 
 server.listen(port, hostname, () => {   //Start the server
-console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+    console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
 })
